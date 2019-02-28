2a4d9448f6b289c77e6ee9210287d2335531c7c3
Ygor Correa Azambuja <ygorazambuja@gmail.com>
2019-02-28 19:16:21 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
-mongoose.connect(
-  "mongodb://usuario1:usuario1@ds121475.mlab.com:21475/escoladb",
-  {
-    useNewUrlParser: true
-  }
-);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true
+});
 app.use(express.json());
 
 app.use(require("./routes"));
 
-app.listen(3000, () => {
-  console.log("Api rodando !! ");
-});
+app.listen(process.env.port || 3000);
