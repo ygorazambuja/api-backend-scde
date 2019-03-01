5a8aadadb901b822e54ba7f8614e89c41bf905f2
Ygor Correa Azambuja <ygorazambuja@gmail.com>
2019-02-28 21:58:39 -0400

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const routes = express.Router();
+const express = require('express')
+const routes = express.Router()
 
-const AlunoController = require("./controller/AlunoController");
+const AlunoController = require('./controller/AlunoController')
+const RecadoController = require('./controller/RecadoController.')
 
-routes.get("/alunos", AlunoController.index);
-routes.post("/postar", AlunoController.store);
-routes.post("/bulk", AlunoController.bulkStore);
+routes.get('/alunos', AlunoController.index)
+routes.post('/postar', AlunoController.store)
+routes.post('/bulk', AlunoController.bulkStore)
 
-module.exports = routes;
+routes.post('/recado', RecadoController.store)
+routes.get('/recados', RecadoController.index)
+
+module.exports = routes
