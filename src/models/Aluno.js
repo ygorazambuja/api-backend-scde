de94682dfa1b4178602de6b0fedc1745bf29f348
Ygor Correa Azambuja <ygorazambuja@gmail.com>
2019-02-28 21:58:36 -0400

    Update Aluno.js

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const AlunoSchema = new mongoose.Schema({
   nome: String,
   serie: String,
   ano: Number
-});
+})
 
-module.exports = mongoose.model("Aluno", AlunoSchema);
+module.exports = mongoose.model('Aluno', AlunoSchema)
