b5a2088748e63525df3503ba036bd8b22a6492ac
Ygor Correa Azambuja <ygorazambuja@gmail.com>
2019-02-28 21:58:32 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express()
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true
-});
-app.use(express.json());
+})
 
-app.use(require("./routes"));
+app.use(express.json())
 
-app.listen(process.env.PORT || 3000);
+app.use(require('./routes'))
+app.use(cors)
+
+app.listen(process.env.PORT || 3000)
