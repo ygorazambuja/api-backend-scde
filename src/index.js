054a5fe9b0779498b9286a50a93a59bb870d0edf
Ygor Azambuja <ygorazambuja@gmail.com>
2019-07-12 16:27:09 -0400

    cors modified

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
 
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 app.use((req, res, next) => {
-  req.io = io
-  return next()
-})
+    req.io = io;
+    return next();
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true
-})
+    useNewUrlParser: true
+});
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(require('./routes'))
-app.use(cors)
+app.use(require("./routes"));
+app.use(cors);
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 3000);
