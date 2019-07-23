5e7a9fd3b12842a5c5a654920fd60c4231989c63
Ygor Azambuja <ygorazambuja@gmail.com>
2019-07-22 22:47:38 -0400

    cors modified

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ app.use(function(req, res, next) {
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, OPTIONS"
+    );
     next();
 });
 
